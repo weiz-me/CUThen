98a2719fde648e0e5c0f1b787e22b86f6b9bb31f
Infernaught <72055086+Infernaught@users.noreply.github.com>
2023-11-17 22:56:01 -0800

    Update LFGetGroup.py

--- LFGetGroup.py
+++ LFGetGroup.py
@@ -2,4 +2,11 @@
 # in the project CUThen, the final project for COMSE6998_010_2023_3,
 # Topics in Computer Science: Cloud Computing and Big Data.
 
-print("Hello World")
+import json
+
+def lambda_handler(event, context):
+    # TODO implement
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
