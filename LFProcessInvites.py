7d4f4124a2cb9a72938a9ec073ae3f64bc609dcc
Chengyu Sun <sun.990502@gmail.com>
2023-11-17 21:03:54 -0500

    add lambda function files

---
+++ LFProcessInvites.py
@@ -0,0 +1,5 @@
+# This file is designed for the lambda function to handle group invites
+# in the project CUThen, the final project for COMSE6998_010_2023_3,
+# Topics in Computer Science: Cloud Computing and Big Data.
+
+print("Hello World")
