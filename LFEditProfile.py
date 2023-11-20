87fe032521a09ee76266f646274067556404af67
Weiz <weizhang45name@gmail.com>
2023-11-19 20:59:27 -0500

    init lf changes

--- LFEditProfile.py
+++ LFEditProfile.py
@@ -7,6 +7,8 @@ import json
 def lambda_handler(event, context):
     # This is a test edit
     # TODO implement
+    print(event)
+    print(context)
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
