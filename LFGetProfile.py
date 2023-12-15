98decfe0a334a1b9dba52f40be87ba1997942608
Chengyu Sun <sun.990502@gmail.com>
2023-12-14 22:31:50 -0500

    add LF

--- LFGetProfile.py
+++ LFGetProfile.py
@@ -3,9 +3,114 @@
 # Topics in Computer Science: Cloud Computing and Big Data.
 
 import json
+import os
+import string
+import boto3
+from opensearchpy import OpenSearch, RequestsHttpConnection
+from requests_aws4auth import AWS4Auth
+from botocore.exceptions import ClientError
+
+REGION = 'us-east-1'
+HOST = 'search-cuthen-temp-5fyo5fvs7x7t2myle4ztwa7swa.us-east-1.es.amazonaws.com'
+INDEX1 = 'user_to_group'
+INDEX2 = 'user_to_inv'
+INDEX3 = 'group_to_user'
+
+def get_awsauth(region, service):
+    cred = boto3.Session().get_credentials()
+    return AWS4Auth(cred.access_key,
+                    cred.secret_key,
+                    region,
+                    service,
+                    session_token=cred.token)
+
+def lookup_data(key, db=None, table='user_table'):
+    if not db:
+        db = boto3.resource('dynamodb')
+    table = db.Table(table)
+    try:
+        response = table.get_item(Key=key)
+    except ClientError as e:
+        print('Error', e.response['Error']['Message'])
+        return False
+    else:
+        ret = response['Item']
+        ret['user_id'] = int(ret['user_id'])
+        return ret
+
+def query(index, field, term):
+    q = {"query": {
+            "bool": {
+                "must": {
+                    "match": {
+                        field: term
+                    }
+                }
+            }
+        }
+    }
+
+    client = OpenSearch(hosts=[{
+        'host': HOST,
+        'port': 443
+    }],
+                        http_auth=get_awsauth(REGION, 'es'),
+                        use_ssl=True,
+                        verify_certs=True,
+                        connection_class=RequestsHttpConnection)
+
+    res = client.search(index=index, body=q)
+    #print(res)
+
+    hits = res['hits']['hits']
+    results = []
+    for hit in hits:
+        results.append(hit['_source'])
+    return results
+
+def get_user(userId):
+    user = {}
+    userInfo = lookup_data(key = userId, table='user_table')
+    user['userId'] = userInfo['user_id']
+    user['userName'] = userInfo['first_name'] + ' ' + userInfo['last_name']
+    user['userFeatures'] = [{k: v} for k, v in userInfo.items()]
+    return user
+
+def get_group(groupId):
+    group = {}
+    gobj = query(index=INDEX3, field='group_id', term=str(groupId))
+    gleader = get_user(gobj['leader_id'])
+    gmember = []
+    for mid in gobj['user_id']:
+        gmember.append(get_user(mid))
+    group['groupId'] = int(groupId)
+    group['groupLeader'] = gleader
+    group['groupMembers'] = gmember
+    return group
 
 def lambda_handler(event, context):
     print(event)
+    userId = event['body']
+    currentUser = get_user(userId)
+    ret = {}
+
+    user_to_group = query(index=INDEX1, field='user_id', term=str(userId))
+    user_to_inv = query(index=INDEX2, field='user_id', term=str(userId))
+    groups = []
+    invs = []
+    for gid in user_to_group['group_id']:
+        groups.append(get_group(gid))
+    
+    for iid in user_to_inv['pending_inv_ids']:
+        invs.append({
+            'invitingGroup': get_group(iid),
+            'invitee': currentUser
+        })
+    ret['groups'] = groups
+    ret['userName'] = currentUser['userName']
+    ret['userFeatures'] = currentUser['userFeatures']
+    ret['pendingInvites'] = invs
+
     # FOR TESTING ONLY
     dummy_response = {
         "groups": [],
@@ -15,5 +120,6 @@ def lambda_handler(event, context):
     }
     return {
         'statusCode': 200,
-        'body': json.dumps(dummy_response)
+        "headers": {"Access-Control-Allow-Origin": "*"},
+        'body': json.dumps(dummy_response) # change to ret when testing
     }
