b9b5202b7bd67c34fec9b411722abbbac3af23ea
Infernaught <89timw@gmail.com>
2023-12-17 02:22:22 -0500

    Fix

--- LFMakeMatch.py
+++ LFMakeMatch.py
@@ -107,7 +107,7 @@ def lambda_handler(event, context):
     print(f"All users: {all_users}")
     print(f"Current user: {currentUser}")
 
-    other_users = [user for user in all_users if int(user['user_id']) != int(currentUser['user_id'])]
+    other_users = [user for user in all_users if int(user['user_id']) != int(currentUser['userId'])]
 
     # FOR TESTING ONLY
     return {
