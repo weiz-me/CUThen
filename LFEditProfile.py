8eaf4bd6e932847be25d15678861632e093d62dd
Infernaught <89timw@gmail.com>
2023-12-17 03:47:58 -0500

    Log

--- LFEditProfile.py
+++ LFEditProfile.py
@@ -31,7 +31,7 @@ def lambda_handler(event, context):
     # }
 
     orginal = lookup_data({'user_id': user_data["currentUser"]}, table="user_table")
-    update_item_list({'user_id':{"N":user_data["currentUser"]}},user_data["newFeatures"],table="user_table")
+    update_item_list({'user_id': user_data["currentUser"]},user_data["newFeatures"],table="user_table")
     updated_Data = lookup_data({'user_id': user_data["currentUser"]}, table="user_table")
 
     response = {"message":"success","input": user_data , "updated_Data":updated_Data}
@@ -67,6 +67,7 @@ def update_item_list(key, feature_dict_list, db=None, table='6998Demo'):
         if feature_name == "user_id":
             continue
         
+        print(f"Key: {key}")
         response = table.update_item(
             Key=key,
             UpdateExpression="set #feature=:f",
