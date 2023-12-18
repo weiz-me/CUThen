610a93b90bdcff1bb7a1037236ffa91507307c79
Chengyu Sun <sun.990502@gmail.com>
2023-12-17 22:03:49 -0500

    update new user creation

--- LFEditProfile.py
+++ LFEditProfile.py
@@ -1,8 +1,14 @@
 import json
 import boto3
+from opensearchpy import OpenSearch, RequestsHttpConnection
+from requests_aws4auth import AWS4Auth
 from botocore.exceptions import ClientError
 from datetime import datetime, timedelta
 import json
+
+REGION = 'us-east-1'
+HOST = 'search-cuthen-temp-5fyo5fvs7x7t2myle4ztwa7swa.us-east-1.es.amazonaws.com'
+INDEX = 'user_to_group'
 
 def lambda_handler(event, context):
     # uni is the primary/paritition key
@@ -32,17 +38,24 @@ def lambda_handler(event, context):
 
     orginal = lookup_data({'user_id': user_data["currentUser"]}, table="user_table")
     if orginal == None:
-        put_item(user_data["currentUser"], user_data["newFeatures"], table="user_table")
-    update_item_list({'user_id': int(user_data["currentUser"])},user_data["newFeatures"],table="user_table")
-    updated_Data = lookup_data({'user_id': user_data["currentUser"]}, table="user_table")
-
-    response = {"message":"success","input": user_data , "updated_Data":updated_Data}
-    resp = {
-            'statusCode': 200,
-            'body': json.dumps(response)
-    }
-    
-    return resp
+        new_id = put_item(user_data["newFeatures"], table="user_table")
+        print(f"new_id: {new_id}")
+        updated_Data = lookup_data({'user_id': new_id}, table="user_table")
+        response = {"message":"insert success","input": user_data , "updated_Data":updated_Data}
+        resp = {
+                'statusCode': 200,
+                'body': json.dumps(response)
+        }
+        return resp
+    else:
+        update_item_list({'user_id': int(user_data["currentUser"])},user_data["newFeatures"],table="user_table")
+        updated_Data = lookup_data({'user_id': user_data["currentUser"]}, table="user_table")
+        response = {"message":"update success","input": user_data , "updated_Data":updated_Data}
+        resp = {
+                'statusCode': 200,
+                'body': json.dumps(response)
+        }
+        return resp
 
 def lookup_data(key, db=None, table='6998Demo'):
     if not db:
@@ -87,11 +100,49 @@ def update_item_list(key, feature_dict_list, db=None, table='6998Demo'):
 
     return
 
-def put_item(id, features, db=None, table='6998Demo'):
+def get_awsauth(region, service):
+    cred = boto3.Session().get_credentials()
+    return AWS4Auth(cred.access_key,
+                    cred.secret_key,
+                    region,
+                    service,
+                    session_token=cred.token)
+
+def query(index, field, term):
+    q = {"query": {
+            "bool": {
+                "must": {
+                    "match": {
+                        field: term
+                    }
+                }
+            }
+        }
+    }
+
+    client = OpenSearch(hosts=[{
+        'host': HOST,
+        'port': 443
+    }],
+                        http_auth=get_awsauth(REGION, 'es'),
+                        use_ssl=True,
+                        verify_certs=True,
+                        connection_class=RequestsHttpConnection)
+
+    res = client.search(index=index, body=q)
+    print(res)
+
+    hits = res['hits']['hits']
+    print(f"hits on max user id: {hits[0]['_source']}")
+    return hits[0]['_source']
+
+def put_item(features, db=None, table='6998Demo'):
     if not db:
         db = boto3.resource('dynamodb')
     table = db.Table(table)
 
+    # get max user id
+    max_id = int(query(index=INDEX, field='type', term='user_id')['max'])
     item = {}
 
     for feature_dict in features:
@@ -100,7 +151,9 @@ def put_item(id, features, db=None, table='6998Demo'):
             'S': feature
         }
     item['user_id'] = {
-        'N': str(id)
+        'N': str(max_id + 1)
     }
-    
+    print(f"item to be inserted into dynamodb: {item}")
+
     response = table.put_item(TableName = table, Item = item)
+    return max_id + 1
